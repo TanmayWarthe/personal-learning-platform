63b018cf1aad7e697e63c3d5d410560bd7a565b6
Tanmay Warthe <tanmaywarthe27@gmail.com>
2025-12-17 23:56:03 +0530

    Initialize backend server and auth routes

--- server/index.js
+++ server/index.js
@@ -3,10 +3,20 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Backend is running");
-});
+const authRoutes = require("./routes/auth.routes");
+app.use("/auth", authRoutes);
 
 app.listen(5000, () => {
   console.log("Server running on port 5000");
+});
+
+
+const pool = require("./config/db");
+
+pool.query("SELECT NOW()", (err, res) => {
+  if (err) {
+    console.error("DB connection error", err);
+  } else {
+    console.log("DB connected at:", res.rows[0].now);
+  }
 });
